8e92ec3588250589a9a4600569b442bbbf8068e0
ewrika <egorkaomsk_2003@mail.ru>
2025-01-29 22:25:48 +0600

    Initial Commit

---
+++ BookShelter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookShelter
+//
+//  Created by Георгий Борисов on 29.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
